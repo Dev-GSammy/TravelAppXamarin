2358ddd53fe7b42692f6862e1e88eccd0e251238
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-04-02 05:38:32 +0100

    Changed the namespace in the homepage, created a new travel page, led the home page tool bar item "add" to the new travel page by invoking the navigation command.

--- TravelAppXamarin/TravelAppXamarin/NewTravelPage.xaml.cs
+++ TravelAppXamarin/TravelAppXamarin/NewTravelPage.xaml.cs
@@ -10,16 +10,11 @@ using Xamarin.Forms.Xaml;
 namespace TravelAppXamarin
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class HomePage : TabbedPage
+    public partial class NewTravelPage : ContentPage
     {
-        public HomePage()
+        public NewTravelPage()
         {
             InitializeComponent();
-        }
-
-        private void ToolbarItem_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new NewTravelPage());
         }
     }
 }
