b338db31d078cc86c1a569d93eab691f53ed5139
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-04-02 04:03:16 +0100

    Add project files.

---
+++ TravelAppXamarin/TravelAppXamarin/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelAppXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Submitbtn_Clicked(object sender, EventArgs e)
+        {
+            bool isName = string.IsNullOrEmpty(NameEntry.Text);
+            bool isPassword = string.IsNullOrEmpty(Password.Text);
+             if (isName || isPassword)
+            {
+                NameEntry.Text = "You didn't fill all fields. Try Again!!!";
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
