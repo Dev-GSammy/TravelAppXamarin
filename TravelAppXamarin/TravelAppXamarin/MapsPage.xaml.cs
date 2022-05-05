43da4fe602283e5ef2a9d6f64b8fcd587ad1d325
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-05-05 12:13:10 +0100

    And I'm done with the map implementation. What may be surprising is that I may not be able to replicate this immediately now. But I believe I'm learning already. it was great implementing the map though. Beautiful things.

--- TravelAppXamarin/TravelAppXamarin/MapsPage.xaml.cs
+++ TravelAppXamarin/TravelAppXamarin/MapsPage.xaml.cs
@@ -7,13 +7,16 @@ using SQLite;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Plugin.Permissions;
+using Plugin.Geolocator;
 using Plugin.Permissions.Abstractions;
+using Plugin.Geolocator.Abstractions;
 
 namespace TravelAppXamarin
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapsPage : ContentPage
     {
+        bool hasLocationPermission = false;
         public MapsPage()
         {
             InitializeComponent();
@@ -44,7 +47,9 @@ namespace TravelAppXamarin
                 }
                 if (status == PermissionStatus.Granted)
                 {
+                    hasLocationPermission = true;
                     locationsMap.IsShowingUser = true;
+                    GetLocation();
                 }
                 else
                 {
@@ -57,6 +62,52 @@ namespace TravelAppXamarin
             }
             #endregion
 
+        }
+
+        //The code below is used to get the exact location of the user automatically even he changes location. The plugin used is the Xam.geolocator
+        //This particular code gets the new location. Next is to move the map to the location result address.   
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            if (hasLocationPermission)
+            {
+                var locator = CrossGeolocator.Current;
+                locator.PositionChanged += Locator_PositionChanged;
+                //To prevent the app from always listening thereby draining battery life, the following code is implemented
+                await locator.StartListeningAsync(TimeSpan.Zero, 100); //zero timespan tells it not to move by time but by distance (100meters)
+            }
+
+                GetLocation();
+        }
+        /*
+         * This method unsubscribes from the ability to listen to distance which can waste battery life.
+         */
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            CrossGeolocator.Current.StopListeningAsync();
+            CrossGeolocator.Current.PositionChanged -= Locator_PositionChanged;
+        }
+
+        private void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            MoveMap(e.Position);
+        }
+        private async void GetLocation()
+        {
+            if (hasLocationPermission)
+            {
+                var locator = CrossGeolocator.Current;
+                var position = await locator.GetPositionAsync();
+                MoveMap(position);
+            }
+        }
+        private void MoveMap(Position position)
+        {
+            //The line below can be written in full if wanted. Function is to move the map to the postion already gotten.
+            var center = new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude);
+            var span = new Xamarin.Forms.Maps.MapSpan(center, 1, 1);
+            locationsMap.MoveToRegion(span);
         }
     }
 }
