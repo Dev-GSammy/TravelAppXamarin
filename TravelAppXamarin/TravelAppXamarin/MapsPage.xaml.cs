8e3aa480dd24c623e4777cf4b812f1d1b2b60e08
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-05-05 05:02:58 +0100

    I'm done implementing the get locaton from the plugin.geolocator stuff.

--- TravelAppXamarin/TravelAppXamarin/MapsPage.xaml.cs
+++ TravelAppXamarin/TravelAppXamarin/MapsPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using SQLite;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 
 namespace TravelAppXamarin
 {
@@ -15,6 +17,46 @@ namespace TravelAppXamarin
         public MapsPage()
         {
             InitializeComponent();
+
+            GetPermissions(); 
+        }
+
+        private async void GetPermissions()
+        {
+            //this particular region will have to be added immediately after the expectation of the plugin writer in the mainActivity and info.plist.
+            //From the way eduardo explained it, it seems quite understandable. But I must say that as of now, I DON'T GET IT. 
+            //I can try to explain it to my self though.
+            #region
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.LocationWhenInUse);
+                if (status != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.LocationWhenInUse))
+                    {
+                        await DisplayAlert("Location", "Please give access to your location", "Okay");
+                    }
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.LocationWhenInUse);
+                    if (results.ContainsKey(Permission.LocationWhenInUse))
+                    {
+                        status = results[Permission.LocationWhenInUse];
+                    }
+                }
+                if (status == PermissionStatus.Granted)
+                {
+                    locationsMap.IsShowingUser = true;
+                }
+                else
+                {
+                    await DisplayAlert("Location", "Please give access to your location", "Okay");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Okay");
+            }
+            #endregion
+
         }
     }
 }
