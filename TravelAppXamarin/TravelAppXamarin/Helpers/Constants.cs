644cb4cd95aef8a9d2352c89dfca6b5aa9604c9c
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-05-10 18:53:54 +0100

    In the venues class at the Model folder, I initiated a method that takes in the latitude and longitude and also passes in the data constants into the method and returns the userless authentication string.

--- TravelAppXamarin/TravelAppXamarin/Helpers/Constants.cs
+++ TravelAppXamarin/TravelAppXamarin/Helpers/Constants.cs
@@ -8,5 +8,8 @@ namespace TravelAppXamarin.Helpers
     {
         //to define the values gotten from the foursquare venues api for userless authentication.
         public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+        public const string CLIENT_ID = "XGFMTTYWYXXKAHYHUYPBBAR3TN2H4DXXGBAKDCREP3D0SWGL";
+        public const string CLIENT_SECRET = "44SMGVN4CGAIKITXOEBNPS33JRWTH0ZABWZIB4HGYJQUD55P";
+        public static string Date = DateTime.Now.ToString("yyyyMMdd");
     }
 }
---
+++ TravelAppXamarin/TravelAppXamarin/Model/Venues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelAppXamarin.Helpers;
+
+namespace TravelAppXamarin.Model
+{
+    public class Venues
+    {
+        public static string GenerateURL(double latitude, double longitude)
+        {
+            
+            return string.Format(Constants.VENUE_SEARCH, latitude, longitude, Constants.CLIENT_ID, Constants.CLIENT_SECRET, Constants.Date);    
+        }
+    }
+}
