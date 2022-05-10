2f6d4dc487e917c3297d06c11322d606ead8914f
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-05-10 18:42:48 +0100

    I created a helpers folder and also created a class in it. The venue search constant created in the class is supposed to hold the userless authentication url with formatted string. which will later be defined in the model folder under the venues class

---
+++ TravelAppXamarin/TravelAppXamarin/Helpers/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelAppXamarin.Helpers
+{
+    public class Constants
+    {
+        //to define the values gotten from the foursquare venues api for userless authentication.
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+    }
+}
