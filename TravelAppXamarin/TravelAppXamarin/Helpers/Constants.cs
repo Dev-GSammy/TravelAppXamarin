5e8bdd41e37923bea07d1d8c66a0f1e7834ea1ba
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-05-31 09:18:48 +0100

    Okay, I think I'm satisfied. We'll work with this query string. I don't like the way the api works tho. You have to be strict with rule when querying the website. Anyways, I found something that worked. Unlike the rubbish Foursquare stuff.

--- TravelAppXamarin/TravelAppXamarin/Helpers/Constants.cs
+++ TravelAppXamarin/TravelAppXamarin/Helpers/Constants.cs
@@ -8,10 +8,10 @@ namespace TravelAppXamarin.Helpers
     { 
         //to define the values gotten from the foursquare venues api for userless authentication.
        
-        public const string VENUE_SEARCH = "https://nominatim.openstreetmap.org/?addressdetails=1&q={0}&format=json&limit=1";
+        public const string VENUE_SEARCH = "https://nominatim.openstreetmap.org/?addressdetails=1&q={0}&format=json&limit=50";
         //public const string CLIENT_ID = "XGFMTTYWYXXKAHYHUYPBBAR3TN2H4DXXGBAKDCREP3D0SWGL";
         //public const string CLIENT_SECRET = "44SMGVN4CGAIKITXOEBNPS33JRWTH0ZABWZIB4HGYJQUD55P";
-        public static string query = "List+of+Universities+in+Nigeria";
+        public static string query = "Universities+in+Nigeria";
         //public static string Date = DateTime.Now.ToString("yyyyMMdd");
     }
 }
