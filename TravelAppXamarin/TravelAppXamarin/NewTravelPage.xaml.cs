1d5d8ed8f7375a19c48ac4d17b6f9462dccffb26
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-06-23 13:03:06 +0100

    I'm done editing the database objects. Now I want to run it. I already put breakpoints in some places. I do hope it reacts well.

--- TravelAppXamarin/TravelAppXamarin/NewTravelPage.xaml.cs
+++ TravelAppXamarin/TravelAppXamarin/NewTravelPage.xaml.cs
@@ -19,55 +19,68 @@ namespace TravelAppXamarin
         {
             InitializeComponent();
         }
-        
+
         protected override async void OnAppearing()
         {
             base.OnAppearing();
 
             var locator = CrossGeolocator.Current;
             var position = await locator.GetPositionAsync();
-            var address =  await VenueLogic.GetVenues();
+            var address = await VenueLogic.GetVenues();
             Console.WriteLine("It has been loaded into the listview page");
             addressListView.ItemsSource = address;
             //var venues = VenueLogic.GetVenues(position.Latitude, position.Longitude);
         }
         //The line below is for the event handler of the button created.
-        private void ToolbarItem_Clicked(object sender, EventArgs e)
+       
+         private void ToolbarItem_Clicked(object sender, EventArgs e)
         {
-            var SelectedVenue = addressListView.SelectedItem as Address;
-            var SelectedUniversity = addressListView.SelectedItem as Universities;
-            
-            //the object created is used to pass and create a new table titled post into the database. with the column experience
-            //The id is created automatically since we already made it autoincrement
-            Post post = new Post()
+            try
             {
-                Experience = ExperienceEntry.Text,
-                AddressAmenity = SelectedVenue.amenity,
-                AddressRoad = SelectedVenue.road,
-                AddressState = SelectedVenue.state,
-                UniversityLatitude = SelectedUniversity.lat,
-                UniversityLongitude = SelectedUniversity.lon
-            };
-            bool ifempty = string.IsNullOrEmpty(ExperienceEntry.Text);
-            //The following lines first creates a connection to the database location already called.
-            //The next line creates the table with the column name as the type. The third line inserts the rows and the we close the connection.
-            if (ifempty)
-            {
-                DisplayAlert("Empty entry","Your entry box is empty, please write your experience","Ok");
-            }
-            else
-            {
-                using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+                var SelectedVenue = addressListView.SelectedItem as Address;
+                var SelectedUniversity = addressListView.SelectedItem as Universities;
+
+                //the object created is used to pass and create a new table titled post into the database. with the column experience
+                //The id is created automatically since we already made it autoincrement
+                Post post = new Post()
                 {
-                    conn.CreateTable<Post>();
-                    int rows = conn.Insert(post);
-                    if (rows > 0)
-                        DisplayAlert("New Row Insert", "New Insert Successful", "Ok");
-                    else
-                        DisplayAlert("New Row Insert", "Insert Unsuccessful", "Ok");
-                    ExperienceEntry.Text = "";
+                    Experience = ExperienceEntry.Text,
+                    AddressAmenity = SelectedVenue.amenity,
+                    AddressRoad = SelectedVenue.road,
+                    AddressState = SelectedVenue.state,
+                    UniversityLatitude = SelectedUniversity.lat,
+                    UniversityLongitude = SelectedUniversity.lon
+                };
+                bool ifempty = string.IsNullOrEmpty(ExperienceEntry.Text);
+                //The following lines first creates a connection to the database location already called.
+                //The next line creates the table with the column name as the type. The third line inserts the rows and the we close the connection.
+                if (ifempty)
+                {
+                    DisplayAlert("Empty entry", "Your entry box is empty, please write your experience", "Ok");
+                }
+                else
+                {
+                    using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+                    {
+                        conn.CreateTable<Post>();
+                        int rows = conn.Insert(post);
+                        if (rows > 0)
+                            DisplayAlert("New Row Insert", "New Insert Successful", "Ok");
+                        else
+                            DisplayAlert("New Row Insert", "Insert Unsuccessful", "Ok");
+                        ExperienceEntry.Text = "";
+                    }
                 }
             }
+            catch(NullReferenceException nre)
+            {
+
+            }
+            catch(Exception ex)
+            {
+
+            }
         }
+
     }
 }
