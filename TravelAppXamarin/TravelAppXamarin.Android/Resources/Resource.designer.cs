4eba2b93dfb50bab983b459ed1d751baf9165a4d
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-07-07 05:19:05 +0100

    Made a few changes in some pages. Can't really tell the changes.

--- TravelAppXamarin/TravelAppXamarin.Android/Resources/Resource.designer.cs
+++ TravelAppXamarin/TravelAppXamarin.Android/Resources/Resource.designer.cs
@@ -24629,13 +24629,31 @@ namespace TravelAppXamarin.Droid
 		{
 			
 			// aapt resource value: 0x7F0C0000
-			public const int icon = 2131492864;
+			public const int add = 2131492864;
 			
 			// aapt resource value: 0x7F0C0001
-			public const int icon_round = 2131492865;
+			public const int history = 2131492865;
 			
 			// aapt resource value: 0x7F0C0002
-			public const int launcher_foreground = 2131492866;
+			public const int home = 2131492866;
+			
+			// aapt resource value: 0x7F0C0003
+			public const int icon = 2131492867;
+			
+			// aapt resource value: 0x7F0C0004
+			public const int icon_round = 2131492868;
+			
+			// aapt resource value: 0x7F0C0005
+			public const int launcher_foreground = 2131492869;
+			
+			// aapt resource value: 0x7F0C0006
+			public const int map = 2131492870;
+			
+			// aapt resource value: 0x7F0C0007
+			public const int profile = 2131492871;
+			
+			// aapt resource value: 0x7F0C0008
+			public const int save = 2131492872;
 			
 			static Mipmap()
 			{
